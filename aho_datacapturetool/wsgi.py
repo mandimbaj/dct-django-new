a6973317c2afa0b6e5f7482412079b244059640d
MANDIMBA Jadice <38686632+mandimbaj@users.noreply.github.com>
2026-06-11 20:06:41 +0100

    Add Django settings configuration for production

--- aho_datacapturetool/wsgi.py
+++ aho_datacapturetool/wsgi.py
@@ -3,9 +3,11 @@ WSGI config for aho_datacapturetooldeveloped for WHO's AFRO iAHO.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 """
-import os
 import pymysql
 pymysql.install_as_MySQLdb()
+
+import os
+
 from django.core.wsgi import get_wsgi_application
 if os.environ.get('DJANGO_ENV') == 'production':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aho_datacapturetool.production')
