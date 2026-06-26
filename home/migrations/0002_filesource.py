285b37f889a0085a1ebae4a19cc770860be6b633
MANDIMBA Jadice <38686632+mandimbaj@users.noreply.github.com>
2026-06-26 16:46:56 +0100

    Change FileSource parent link to data_wizard

--- home/migrations/0002_filesource.py
+++ home/migrations/0002_filesource.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='FileSource',
             fields=[
-                ('filesource_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='sources.FileSource')),
+                ('filesource_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='data_wizard.FileSource')),
                 ('url', models.CharField(blank=True, max_length=255, null=True)),
                 ('location', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, to='regions.StgLocation', verbose_name='Location Name')),
             ],
